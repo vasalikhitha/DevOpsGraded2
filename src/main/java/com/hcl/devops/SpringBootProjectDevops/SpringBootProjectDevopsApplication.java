2bfeacbfb70562131e925ea45045ed86bb73fcb8
vasalikhitha <likhithavasa1077@gmail.com>
2021-06-06 18:42:01 +0530

    initial commit

---
+++ src/main/java/com/hcl/devops/SpringBootProjectDevops/SpringBootProjectDevopsApplication.java
@@ -0,0 +1,13 @@
+package com.hcl.devops.SpringBootProjectDevops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProjectDevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProjectDevopsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hcl/devops/SpringBootProjectDevops/SpringBootProjectDevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcl.devops.SpringBootProjectDevops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootProjectDevopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
